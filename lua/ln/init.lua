947ad6186041769388e6b170ab4796735e6df8cc
Markos Narinian <manarinian@gmail.com>
2026-05-26 17:23:50 +0300

    Call redraw() on CmdlineEnter/Leave via shared autocmds

--- lua/ln/init.lua
+++ lua/ln/init.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 M.config = {
-	absolute_events = { "WinLeave", "InsertEnter", "CmdlineEnter", "TermEnter" },
-	relative_events = { "WinEnter", "InsertLeave", "CmdlineLeave", "TermLeave" },
+	absolute_events = { "WinLeave", "InsertEnter", "TermEnter", "CmdlineEnter" },
+	relative_events = { "WinEnter", "InsertLeave", "TermLeave", "CmdlineLeave" },
 }
 
 function M.setup(opts)
@@ -14,15 +14,18 @@ vim.o.relativenumber = false
 vim.wo.relativenumber = true
 
 vim.api.nvim_create_autocmd(M.config.absolute_events, {
-	callback = function()
+	callback = function(ev)
 		vim.wo.number = vim.go.number
 		vim.wo.relativenumber = false
+		if ev.event == "CmdlineEnter" then vim.cmd("redraw") end
 	end,
 })
+
 vim.api.nvim_create_autocmd(M.config.relative_events, {
-	callback = function()
+	callback = function(ev)
 		vim.wo.number = vim.go.number
 		vim.wo.relativenumber = true
+		if ev.event == "CmdlineLeave" then vim.cmd("redraw") end
 	end,
 })
 
